055bd8556ecdf774bcb1624359dff69c2edd8b82
Sami Eria <seria@esri.com>
2015-05-19 18:33:18 -0700

    How to detect pyramids in a raster dataset
    How to detect pyramids in a raster dataset

---
+++ arcobjects-net/pyramids-present-in-raster-dataset/Readme.md
@@ -0,0 +1,11 @@
+# How to programmtically discover that there are pyramids in a raster dataset
+This code sample shows how to programmtically discover that there are pyramids in a raster dataset.
+Author: Sami E.
+
+[Documentation on IAuthorizeLicense]
+(http://resources.arcgis.com/en/help/arcobjects-net/componenthelp/index.html#//00420000031q000000)
+
+## Features
+* Uses IAuthorizeLicense
+* Uses IAoInitialize
+
---
+++ arcobjects-net/pyramids-present-in-raster-dataset/_IRasterPyramid3_Present.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.DataSourcesRaster;
+using ESRI.ArcGIS.Geodatabase;
+using System.Windows.Forms;
+
+namespace Addin_IRasterPyramid3_Present
+{
+    public class _IRasterPyramid3_Present : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public _IRasterPyramid3_Present()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            Type factoryType = Type.GetTypeFromProgID("esriDataSourcesRaster.RasterWorkspaceFactory");
+            IWorkspaceFactory wsFactory = (IWorkspaceFactory)Activator.CreateInstance(factoryType);
+            string filePath = @"C:\Temp\raster\"; // NAme of File = LC80400362014321LGN00.jpg
+            IWorkspace workspace = wsFactory.OpenFromFile(filePath, 0);
+            IRasterWorkspace rasterWorkspace = workspace as IRasterWorkspace;
+            IRasterDataset rasterDataset = rasterWorkspace.OpenRasterDataset("Landsat_8_Redlands_LC80400362013142LGN01.jpg");
+
+            IRasterPyramid3 rasterPyramid3 = rasterDataset as IRasterPyramid3;
+            bool presentBool = rasterPyramid3.Present;
+            MessageBox.Show("presentBool = " + presentBool);
+
+            ArcMap.Application.CurrentTool = null;
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
