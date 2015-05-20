de5b15bf522deed9b27afe7a33316608e2591e54
Sami Eria <seria@esri.com>
2015-05-19 17:30:52 -0700

    How to Authorize and De-Authorize ESLF Licenses
    How to Authorize and De-Authorize ESLF Licenses

---
+++ arcobjects-net/ESLF-license-authorization-and-deauthorization/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESRI.ArcGIS;
+using ESRI.ArcGIS.esriSystem;
+
+
+namespace ConsoleApplication_ESLF_License
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ESRI.ArcGIS.RuntimeManager.Bind(ProductCode.Engine);
+
+            // 1. Authorize the ESLF license
+            ESRI.ArcGIS.esriSystem.IAuthorizeLicense aoAuthorizeLicense = new AoAuthorizeLicenseClass();
+
+            try
+            {
+                string pathToLicenseFile = @"C:\temp\EngineTest.Eslf";
+                string password = "";
+
+                aoAuthorizeLicense.AuthorizeASRFromFile(pathToLicenseFile, password);
+
+                string featuresAdded = aoAuthorizeLicense.FeaturesAdded;
+                System.Diagnostics.Debug.WriteLine("Features Added: " + featuresAdded);
+		
+		// 2. De-Authorize ESLF License
+                aoAuthorizeLicense.DeauthorizeASRFromFile(pathToLicenseFile, password);
+
+                featuresAdded = aoAuthorizeLicense.FeaturesAdded;
+                System.Diagnostics.Debug.WriteLine("Features Added: " + featuresAdded);
+            }
+            catch (Exception e)
+            {
+                string lastError;
+                int errorNumber = aoAuthorizeLicense.get_LastError(out lastError);
+                System.Diagnostics.Debug.WriteLine("Error Number " + errorNumber + " : " + e.Message + ". Last Error: " + lastError);
+            }
+
+           
+
+            // Check if the Engine Product Code is available
+            ESRI.ArcGIS.esriSystem.IAoInitialize aoInitialize = new AoInitialize();
+            ESRI.ArcGIS.esriSystem.esriLicenseStatus esriLicense_Status = aoInitialize.IsProductCodeAvailable(esriLicenseProductCode.esriLicenseProductCodeEngine);
+            System.Diagnostics.Debug.WriteLine("esriLicenseStatus: " + esriLicense_Status);
+
+        }
+    }
+}
---
+++ arcobjects-net/ESLF-license-authorization-and-deauthorization/Readme.md
@@ -0,0 +1,11 @@
+# Authorize and de-authorize ESRI Secure File License Files (ESLF).
+This code sample shows how to authorize and de-authorize ESLF license files.
+Author: Sami E.
+
+[Documentation on IAuthorizeLicense]
+(http://resources.arcgis.com/en/help/arcobjects-net/componenthelp/index.html#//00420000031q000000)
+
+## Features
+* Uses IAuthorizeLicense
+* Uses IAoInitialize
+
