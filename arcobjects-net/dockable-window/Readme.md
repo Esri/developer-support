ed39766abc72d3b2456ee55d08dd877ea6e85e64
Sami Eria <seria@esri.com>
2015-09-08 16:35:31 -0700

    Added the MD file for the Dockable Window sample
    Added the MD file for the Dockable Window sample

---
+++ arcobjects-net/dockable-window/Readme.md
@@ -0,0 +1,12 @@
+# How to create a dockable window in an an ArcMap Add-in project, and turn it off/on
+This code sample shows how to create a dockable window in an an ArcMap Add-in project, and also shows how to make it
+visible/invisible. 
+Author: Sami E.
+
+[Documentation on Dockable Windows]
+(http://resources.arcgis.com/en/help/arcobjects-net/conceptualhelp/#/Creating_dockable_windows/00010000014w000000/)
+
+## Features
+* Uses IDockableWindow
+* Uses IDockableWindowManager
+* Uses UID
---
+++ arcobjects-net/dockable-window/TurnOnOff_DocWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.esriSystem;
+
+
+namespace ArcMapAddin_DocWindow
+{
+    public class TurnOnOff_DocWindow : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public TurnOnOff_DocWindow()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            UID uid = new UID();
+            uid.Value = ThisAddIn.IDs._DocWindowSami; // ThisAddin.IDs returns all the class IDs under this Add-in project
+            IDockableWindowManager dockWindowManager = ArcMap.Application as IDockableWindowManager;
+            IDockableWindow dockableWindow = dockWindowManager.GetDockableWindow(uid);
+            //IDockableWindow dockableWindow = GetDockableWindow(ArcMap.Application, "esriGeoprocessingUI.GPCommandWindow"); // Open a System dockable window
+            dockableWindow.Show(true); // use False to hide the dockable window
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
