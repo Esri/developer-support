22798719d6e8344cb56d263b2382c0895b8f89ee
Ashley Stalzer <astalzer@esri.com>
2015-09-18 18:27:25 -0700

    Update community.py
    added communitySelf

--- python/general-python/agol-helper/community.py
+++ python/general-python/agol-helper/community.py
@@ -1,28 +1,45 @@
 from agol import AGOL
 
 class community(AGOL):
-    """Community object, that inherits properties from the AGOL object."""
+    """
+    Community object that contains operations related to users and groups, \
+    and inherits properties from the AGOL object.
+    """
 
     def groupSearch(self):
-        """The Group Search operation searches for groups in the portal:
-        http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#//02r3000000m1000000"""
+        """
+        The Group Search operation searches for groups in the portal:
+        http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#//02r3000000m1000000
+        """
         url = "http://{short}.maps.arcgis.com/sharing/rest/community/groups".format(short = self.short)
+        print(url)
         data = {'token': self.token,
                 'f': 'json',
+                'num': 15,
                 'q': 'orgid:{orgID}'.format(orgID = self.orgID)}
         jsonResponse = self.sendRequest(url, data)
-        results = jsonResponse['results']
-        orgGroups = [(x['id'], x['title']) for x in results]
-        return orgGroups
+        return jsonResponse
 
     def userSearch(self):
-        """The User Search operation searches for users in the portal.
-        http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/User_Search/02r3000000m6000000/"""
+        """
+        The User Search operation searches for users in the portal.
+        http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/User_Search/02r3000000m6000000/
+        """
         url = "http://{short}.maps.arcgis.com/sharing/rest/community/users".format(short = self.short)
         data = {'token': self.token,
                 'f': 'json',
+                'num': 15,
                 'q': 'orgid:{orgID}'.format(orgID = self.orgID)}
         jsonResponse = self.sendRequest(url, data)
-        results = jsonResponse['results']
-        users = [x['username'] for x in results]
-        return users
+        return jsonResponse
+
+    def communitySelf(self):
+        """
+        This resource allows discovery of the current authenticated user identified by the token.
+        http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/Self/02r300000079000000/
+        """
+        url = "http://{short}.maps.arcgis.com/sharing/rest/community/self".format(short = self.short)
+        data = {'token': self.token,
+                'f': 'json'}
+        jsonResponse = self.sendRequest(url, data)
+        return jsonResponse
