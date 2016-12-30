24f9ae2c293848def851c44e5ec006d651d0a497
Alexander Nohe <nohe427@users.noreply.github.com>
2016-12-30 15:24:51 -0500

    Passive Location Retrieval Sample Added (#274)

---
+++ runtime-android/passive-location-retrieval/PassiveLocationUpdater/app/src/main/java/com/arcgis/androidsupportcases/passivelocationupdater/LocationUpdateUtilities.java
@@ -0,0 +1,12 @@
+package com.arcgis.androidsupportcases.passivelocationupdater;
+
+/**
+ * Created by alex7370 on 10/10/2016.
+ */
+
+public class LocationUpdateUtilities {
+    public static final String PRIVATE_BROADCAST_PREMISSION =
+            "com.arcgis.androidsupportcases.passivelocationupdater.Private";
+    public static final String NEW_LOCATION =
+            "com.arcgis.androidsupportcases.passivelocationupdater.NEW_LOCATION";
+}
