b53958f5c4c953cf06a058a06b87938598b1107a
Alexander Nohe <alex.nohe427@gmail.com>
2017-10-08 13:30:33 -0400

    Add README.md
    Added a readme and updated the layout to focus on username first.

--- runtime-android/authentication-challenge/authentication-challenge/app/src/main/java/com/thosedevs/authenticationchallenger/MainActivity.kt
+++ runtime-android/authentication-challenge/authentication-challenge/app/src/main/java/com/thosedevs/authenticationchallenger/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.thosedevs.authenticationchallenger
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.app.DialogFragment
-import android.content.Context
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
-import com.esri.arcgisruntime.data.ArcGISFeatureTable
+import android.widget.EditText
+import android.widget.Toast
 import com.esri.arcgisruntime.layers.ArcGISMapImageLayer
-import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
 import com.esri.arcgisruntime.security.*
 import kotlinx.android.synthetic.main.activity_main.*
-import android.view.LayoutInflater
-import android.widget.EditText
-import android.widget.Toast
-import kotlinx.android.synthetic.main.login_dialog_layout.*
 import java.util.concurrent.CountDownLatch
 
 
@@ -41,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         secureLayer.addDoneLoadingListener({Log.e("NOHE", "DONE")})
     }
 
-    class CustomAuthenticationChallengeHandler(context: Activity?) : DefaultAuthenticationChallengeHandler(context),
+    class CustomAuthenticationChallengeHandler(val context: Activity?) : DefaultAuthenticationChallengeHandler(context),
     LoginDialog.LoginDialogListener {
         override fun onDialogClick(dialog: DialogFragment) {
             when(dialog) {
@@ -49,27 +44,27 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val context = context
         var countDownLatch = CountDownLatch(0)
         override fun handleChallenge(challenge: AuthenticationChallenge?): AuthenticationChallengeResponse {
-            if (challenge?.type == AuthenticationChallenge.Type.USER_CREDENTIAL_CHALLENGE) {
+            return if (challenge?.type == AuthenticationChallenge.Type.USER_CREDENTIAL_CHALLENGE) {
                 countDownLatch = CountDownLatch(1)
                 val loginDialog = LoginDialog(context, this)
                 loginDialog.show(context?.fragmentManager, "Challenger")
                 countDownLatch.await()
-                return loginDialog.authChallengeResponse
+                loginDialog.authChallengeResponse
             } else {
-                return super.handleChallenge(challenge)
+                super.handleChallenge(challenge)
             }
         }
 
 
     }
 
+    @SuppressLint("ValidFragment")
     class LoginDialog(val context: Activity?,
-                      val challengeHandler: LoginDialog.LoginDialogListener) : DialogFragment() {
+                      challengeHandler: LoginDialog.LoginDialogListener) : DialogFragment() {
 
-        var mListener: LoginDialogListener = challengeHandler
+        private var mListener: LoginDialogListener = challengeHandler
 
         lateinit var authChallengeResponse: AuthenticationChallengeResponse
 
@@ -77,8 +72,9 @@ class MainActivity : AppCompatActivity() {
             val builder = AlertDialog.Builder(context)
             val inflater = activity.layoutInflater
             val dialogView = inflater.inflate(R.layout.login_dialog_layout, null)
-            var password: EditText = dialogView.findViewById(R.id.password)
-            var username: EditText = dialogView.findViewById(R.id.username)
+            val password: EditText = dialogView.findViewById(R.id.password)
+            val username: EditText = dialogView.findViewById(R.id.username)
+
             builder.setView(dialogView)
                     .setPositiveButton("Sign in", { _dialogInterface, _id ->
                         if(!username.text.isNullOrBlank() && !password.text.isNullOrBlank()) {
@@ -87,14 +83,17 @@ class MainActivity : AppCompatActivity() {
                                     UserCredential(username.text.toString(), password.text.toString()))
                             mListener.onDialogClick(this)
                         } else {
-                            Toast.makeText(context, "Username / Password blank", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(context, "Username / Password blank", Toast.LENGTH_SHORT)
+                                    .show()
                         }
 
                     })
                     .setNegativeButton("Cancel", {
                         dl, _ ->
-                        authChallengeResponse = AuthenticationChallengeResponse(AuthenticationChallengeResponse.Action.CANCEL,
-                                null)
+                        authChallengeResponse = AuthenticationChallengeResponse(
+                                AuthenticationChallengeResponse.Action.CANCEL,
+                                null
+                        )
                         this.dialog.cancel()
                         mListener.onDialogClick(this)
                     })
@@ -104,7 +103,6 @@ class MainActivity : AppCompatActivity() {
         interface LoginDialogListener {
             fun onDialogClick(dialog: DialogFragment)
         }
-
     }
 
 
