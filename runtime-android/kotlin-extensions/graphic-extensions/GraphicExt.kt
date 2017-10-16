7702a229650c843f8d8bcfcd72c22e6dfa4cb54f
Alexander Nohe <nohe427@users.noreply.github.com>
2017-10-16 09:24:29 -0400

    Pulse Graphic Size (#317)

--- runtime-android/kotlin-extensions/graphic-extensions/GraphicExt.kt
+++ runtime-android/kotlin-extensions/graphic-extensions/GraphicExt.kt
@@ -28,3 +28,42 @@ fun Graphic.animatePointGraphic(handler: Handler, destination: Point) {
         }
     })
 }
+
+/**
+ * The puleGraphicSize method allows developers to pulse a graphic size to show the graphic
+ * changing and getting larger and smaller between a range.
+ */
+fun Graphic.pulseGraphicSize(period: Long, min: Float = 2.0f, max: Float = 20.0f) {
+    val timer = Timer("pulse", false)
+    timer.scheduleAtFixedRate(SizePulseTimer(this.symbol, min, max), 0, period)
+}
+
+/**
+ *
+ * This class is used exclusively with the pulseGraphicSize extension function.  This allows
+ * developers to easily pulse a graphic size.
+ *
+ */
+private class SizePulseTimer(val symbol: Symbol, val min: Float = 2.0f, val max: Float = 20.0f) : TimerTask() {
+
+    var size = 5.0f
+    var goingDown = true
+
+    override fun run() {
+        when(symbol) {
+            is SimpleMarkerSymbol -> symbol.size += (if (goingDown) (-.01f) else (.01f)).apply { size = symbol.size }
+            is PictureMarkerSymbol -> {
+                symbol.height += (if (goingDown) (-.01f) else (.01f))
+                symbol.width += (if (goingDown) (-.01f) else (.01f)).apply { size = symbol.height }
+            }
+            is SimpleLineSymbol -> symbol.width += (if (goingDown) (-.01f) else (.01f)).apply { size = symbol.width }
+            is SimpleFillSymbol -> symbol.outline.width += (if (goingDown) (-.01f) else (.01f)).apply { size = symbol.outline.width }
+        }
+
+        if (size < min) {
+            goingDown = false
+        } else if (size > max) {
+            goingDown = true
+        }
+    }
+}
--- runtime-android/kotlin-extensions/graphic-extensions/README.md
+++ runtime-android/kotlin-extensions/graphic-extensions/README.md
@@ -11,3 +11,9 @@ Append your extension to the `GraphicExt.kt` file and then post a short descript
 Animates a marker to a new location smoothly.  This prevents the marker from jumping to a new location.
 
 Amended for Esri based on this [stack overflow post](https://stackoverflow.com/questions/16338774/show-a-moving-marker-on-the-map)
+
+### pulseGraphicSize(period: Long, min: Float = 2.0f, max: Float = 20.0f)
+Pulses a graphic in size to help alert users that the graphic is selected.
+
+### private class SizePulseTimer(val symbol: Symbol, val min: Float = 2.0f, val max: Float = 20.0f) : TimerTask()
+Used exclusively for the pulse graphic size method.  This handles running the graphic size adjustment logic in a [TimerTask()](https://developer.android.com/reference/java/util/TimerTask.html).
