9851e436ad80a841a2bf02c56de63936b13c6405
Ken Galliher <natas333x2@gmail.com>
2018-05-01 13:48:50 -0700

    Add Pro DB Test
    Add full VS solution

---
+++ pro-sdk/database-connection-tester/GeodatabaseConnection/Dockpane1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace GeodatabaseConnection
+{
+    /// <summary>
+    /// Interaction logic for Dockpane1View.xaml
+    /// </summary>
+    public partial class Dockpane1View : UserControl
+    {
+        public Dockpane1View()
+        {
+            InitializeComponent();
+        }
+    }
+}
