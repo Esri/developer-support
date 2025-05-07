e74c68d4b436272313ef04bba21efe93f1e4dcfb
Paul Gibbs <123662740+PaulGibbs3rd@users.noreply.github.com>
2025-05-07 10:30:46 -0700

    added createCurve pro sdk button sample
    Sample implements https://pro.arcgis.com/en/pro-app/3.3/sdk/api-reference/topic8050.html creating 4 curves in the city of seattle with 4 different color.

---
+++ pro-sdk/createCurve/Button1.cs
@@ -0,0 +1,69 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Core.Internal.Geometry;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+
+namespace createCurve
+{
+    internal class Button1 : Button
+    {
+        protected override void OnClick()
+        {
+            QueuedTask.Run(() =>
+            {
+                // Projected spatial reference in meters
+                var webMercator = SpatialReferences.WebMercator;
+
+                // Seattle coordinates (WGS84)
+                var seattleWgs = MapPointBuilder.CreateMapPoint(-122.3321, 47.6062, SpatialReferences.WGS84);
+
+                // Project to Web Mercator (meters)
+                var startPoint = (MapPoint)GeometryEngine.Instance.Project(seattleWgs, webMercator);
+
+                // Shared arc parameters
+                double radius = 1000;
+                double chordLength = 800;
+
+                // Define bearings and colors
+                var configs = new List<(double bearing, CIMColor color)>
+                {
+                    (0 * 0.01745327777,    ColorFactory.Instance.RedRGB),
+                    (45 * 0.01745327777,   ColorFactory.Instance.GreenRGB),
+                    (90 * 0.01745327777,   ColorFactory.Instance.BlueRGB),
+                    (180 * 0.01745327777,  ColorFactory.Instance.CreateRGBColor(255, 165, 0)) // Orange
+                };
+
+                foreach (var (bearing, color) in configs)
+                {
+                    // Create arc
+                    var arc = EllipticArcBuilderEx.CreateCircularArc(
+                        startPoint,
+                        chordLength,
+                        bearing,
+                        radius,
+                        ArcOrientation.ArcCounterClockwise,
+                        MinorOrMajor.Minor,
+                        webMercator);
+
+                    var polyline = PolylineBuilderEx.CreatePolyline(arc);
+
+                    // Create symbol
+                    var lineSymbol = SymbolFactory.Instance.ConstructLineSymbol(color, 2.0, SimpleLineStyle.Solid);
+                    var symbolRef = new CIMSymbolReference { Symbol = lineSymbol };
+
+                    // Add overlay to map
+                    MapView.Active?.AddOverlay(polyline, symbolRef);
+                }
+
+                // Optional: zoom to show the curves clearly
+                MapView.Active?.ZoomTo(startPoint.Extent.Expand(1000, 1000, false), TimeSpan.FromSeconds(0.5));
+
+            });
+        }
+    }
+}
---
+++ pro-sdk/createCurve/Module1.cs
@@ -0,0 +1,48 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.KnowledgeGraph;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace createCurve
+{
+    internal class Module1 : Module
+    {
+        private static Module1 _this = null;
+
+        /// <summary>
+        /// Retrieve the singleton instance to this module here
+        /// </summary>
+        public static Module1 Current => _this ??= (Module1)FrameworkApplication.FindModule("createCurve_Module");
+
+        #region Overrides
+        /// <summary>
+        /// Called by Framework when ArcGIS Pro is closing
+        /// </summary>
+        /// <returns>False to prevent Pro from closing, otherwise True</returns>
+        protected override bool CanUnload()
+        {
+            //TODO - add your business logic
+            //return false to ~cancel~ Application close
+            return true;
+        }
+
+        #endregion Overrides
+
+    }
+}
