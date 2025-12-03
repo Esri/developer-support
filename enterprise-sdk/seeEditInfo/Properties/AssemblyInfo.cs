d50420ef7ba0baa61e1193e0683d5d99c58fa364
Paul Gibbs <pau11750@esri.com>
2025-12-03 13:44:06 -0800

    Added seeEditInfo SOE source code and documentation

---
+++ enterprise-sdk/seeEditInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("seeEditInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ESRI")]
+[assembly: AssemblyProduct("seeEditInfo")]
+[assembly: AssemblyCopyright("Copyright © ESRI 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b021abad-83d4-4510-982b-ed9d95e7fc66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: ESRI.Server.SOESupport.AddInPackage("seeEditInfo", "cfdb6d8a-28d0-429e-9af0-0d43564e54e6",
+    Author = "pau11750",
+    Company = "ESRI",
+    Date = "12/1/2025 12:46:14 PM",
+    Description = "",
+    TargetProduct = "Server",
+    TargetVersion = "11.5",
+    Version = "1.0")]
